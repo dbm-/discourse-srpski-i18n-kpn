a452917e52531f2187a7dea23d4290af8f4a185e
dbm <bashmpq@gmail.com>
2015-02-05 18:15:17 +0100

    Promena bash coda

--- reload_i18n.sh
+++ reload_i18n.sh
@@ -2,7 +2,7 @@
 # A simple script to reload translations from github
 
 cd /tmp
-git clone https://github.com/taypo/discourse-srpski-i18n-kpn.git
+git clone https://github.com/dbm-/discourse-srpski-i18n-kpn.git
 cp -rT discourse-srpksi-i18n-kpn/ /var/www/discourse/
 cp -f discourse-srpski-i18n-kpn/reload_i18n.sh /root/reload_i18n.sh
 chmod +x /root/reload_i18n.sh
